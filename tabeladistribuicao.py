c3ea032e34b5cfd8870822cc5dd53ffbe49a295b
Leonardo Min Woo Chung <127551335+LeonardoChung@users.noreply.github.com>
2025-03-03 10:19:15 -0300

    tratamento de strings / textos

--- tabeladistribuicao.py
+++ tabeladistribuicao.py
@@ -5,34 +5,40 @@ import pandas as pd
 df = pd.read_csv("Netflix Engagement Dataset.csv")
 
 def tabela_distributiva(coluna):
-    n = len(df[coluna])
-    A = round(max(df[coluna]) - min(df[coluna]))  # Amplitude = maior - menor
-    i = round(1 + 3.3 * np.log10(n))              # Quantidade de classes
-    h = round(A / i)                              # Amplitude classe
-
-    menor = min(df[coluna])
-    intervalos = []
-    fi = []
-    xi = []
-
-    while menor <= max(df[coluna]):
-        maior = menor + h
-        intervalo = f"{menor} - {maior}"  # Intervalos
-        intervalos.append(intervalo)
-        freq = len(df[(df[coluna] >= menor) & (df[coluna] < maior)])
-        fi.append(freq) # Frequência
-        xi.append((menor + maior) / 2) # Ponto médio 
-        menor = maior
-
-    Fi = np.cumsum(fi) # Frequência acumulada
-
-    # Gera a tabela
-    tabela = pd.DataFrame(
-        data= zip(intervalos, fi, xi, Fi),
-        columns=["intervalo", "frequência", "ponto médio", "frequência acumulada"]
-        )
+    if df[coluna].dtype == 'object': # String
+        freq = df[coluna].value_counts() # Contagem dos valores
+        porcentagem = df[coluna].value_counts(normalize=True) * 100 # Porcentagem dos valores
+        tabela = pd.DataFrame({'frequência': freq, 'percentual (%)': porcentagem})
+        
+    else:    
+        n = len(df[coluna])
+        A = round(max(df[coluna]) - min(df[coluna]))  # Amplitude = maior - menor
+        i = round(1 + 3.3 * np.log10(n))              # Quantidade de classes
+        h = round(A / i)                              # Amplitude classe
+    
+        menor = min(df[coluna])
+        intervalos = []
+        fi = []
+        xi = []
+    
+        while menor <= max(df[coluna]):
+            maior = menor + h
+            intervalo = f"{menor} - {maior}"  # Intervalos
+            intervalos.append(intervalo)
+            freq = len(df[(df[coluna] >= menor) & (df[coluna] < maior)])
+            fi.append(freq) # Frequência
+            xi.append((menor + maior) / 2) # Ponto médio 
+            menor = maior
+    
+        Fi = np.cumsum(fi) # Frequência acumulada
+    
+        # Gera a tabela
+        tabela = pd.DataFrame(
+            data= zip(intervalos, fi, xi, Fi),
+            columns=["intervalo", "frequência", "ponto médio", "frequência acumulada"]
+            )
     print(tabela)
 
 # testando
-coluna = 'Customer Satisfaction Score (1-10)'
+coluna = 'Promotional Offers Used'
 tabela_distributiva(coluna)
