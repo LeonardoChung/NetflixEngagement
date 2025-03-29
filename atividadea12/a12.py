a7f81eeaa20f6123172ff24e1e23b3d74243684c
Leonardo Min Woo Chung <127551335+LeonardoChung@users.noreply.github.com>
2025-03-29 11:10:47 -0300

    a12

--- atividadea12/a12.py
+++ atividadea12/a12.py
@@ -1,1 +1,36 @@
+import pandas as pd
 
+df = pd.read_csv("Netflix Engagement Dataset.csv")
+
+estatisticas = {}
+
+for coluna in df.columns:
+    if df[coluna].dtype in ['int64', 'float64']: # numericas
+        media = df[coluna].mean()
+        mediana = df[coluna].median()
+        moda = df[coluna].mode()
+        if not moda.empty:  
+            moda = moda.tolist() 
+        else:  
+            moda = "Não há moda"
+    
+    elif df[coluna].dtype == 'object': # categoricas
+        media = "Não é possível calcular em variáveis categóricas"
+        mediana = "Não é possível calcular em variáveis categóricas"
+        moda = df[coluna].mode()
+        if not moda.empty:  
+            moda = moda.tolist()
+        else:  
+            moda = "Não há moda"
+    
+    else:
+        media = "Tipo de dado não suportado"
+        mediana = "Tipo de dado não suportado"
+        moda = "Tipo de dado não suportado"
+    
+    estatisticas[coluna] = {"Média": media, "Mediana": mediana, "Moda": moda}
+
+# inverte linha e coluna para melhor visualzacaoe
+estatisticas_df = pd.DataFrame(estatisticas).T
+
+print(estatisticas_df)
